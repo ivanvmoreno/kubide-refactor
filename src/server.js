82331448cddf604be308a298f1ff8721f702a374
Iván Vicente Moreno Cencerrado <contacto@ivanmoreno.me>
2018-10-29 02:06:05 +0100

    Mejora de comentarios del código

--- src/server.js
+++ src/server.js
@@ -1,10 +1,13 @@
 'use strict';
 
+// Importamos la función traducirTexto(texto, idioma [, next])
 const translate = require('./translator');
 
+// Definimos el texto original y el idioma al que queremos traducirlo
 var texto = "Quiero trabajar con los mejores",
     idioma = 'en';
 
+// Ejecutamos la función y pasamos una callback con error handling
 translate.traducirTexto(texto, idioma, (err, resultado) => {
     if (err) {
         console.log(err);
